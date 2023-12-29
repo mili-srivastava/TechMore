9227407f720f2cb9d59e73232729e8bb91899690
Mili srivastava <milisrivastava42@gmail.com>
2023-12-29 12:20:05 +0530

    changes in profile page

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,12 +1,12 @@
 import ProfileContent from "@/components/profile/ProfileContent";
-import { userData } from "@/types";
+
 
 const page = ({
   params,
 }: {
   params: {
     id: string;
-  },userData:userData
+  },
 }) => {
   console.log(params);
   return (
